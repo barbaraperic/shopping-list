a599cc7ad4a67f89c9ba5932fc65b1f846fb9d17
Barbara Peric <b.peric10@gmail.com>
2021-06-09 16:02:47 +0200

    FEAT: tooltip

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -6,14 +6,14 @@ const Tabs = () => {
     <List>
       <ListItem>
         <StyledIcon id="list" />
-        <Tooltip text="list" />
-        <Spacer />
+        <Tooltip text="items" />
       </ListItem>
+      <Spacer />
       <ListItem>
         <StyledIcon id="rotate"/>
-        <Tooltip text="rotate" />
-        <Spacer />
+        <Tooltip text="history" />
       </ListItem>
+      <Spacer />
       <ListItem>
         <StyledIcon id="bar"/>
         <Tooltip text="statistics" />
@@ -34,18 +34,39 @@ const List = styled.ul`
 
 const ListItem = styled.li`
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const TooltipWrapper = styled.span`
   visibility: hidden;
+  position: absolute;
+  color: white;
+  font-size: 12px;
+  background-color: #454545;
+  padding: 4px 10px 4px 8px;
+  left: 102px;
+  border-radius: 4px;
 
   &::after {
     content: '';
+    position: absolute;
+    left: 0;
+    top: 125%;
+    width: 0;
+    height: 0;
+    border: 5px solid transparent;
+    border-right-color: #454545;
+    border-left: 0;
+    margin-top: -20px;
+    margin-left: -5px;
   }
+
 `
 
 const StyledIcon = styled(NavIcon)`
-  position: relative;
+  /* position: relative; */
 
   &:hover + ${TooltipWrapper} {
     visibility: visible;
