8740cd5ae4704cd9c1a03f86d96408cb09877ded
Barbara Peric <b.peric10@gmail.com>
2021-06-11 09:34:13 +0200

    FEAT: select button

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,8 @@ const Button = ({ children, variant, className }) => {
     Component = SecondaryButton
   } else if (variant === 'warning') {
     Component = WarningButton
+  } else if (variant === 'select') {
+    Component = SelectButton
   } else {
     Component = GhostButton
   }
@@ -17,8 +19,6 @@ const Button = ({ children, variant, className }) => {
 }
 
 const ButtonBase = styled.button`
-  /* height: 62px;
-  min-width: 87px; */
   border-radius: 12px;
   padding: 16px 24px;
   color: white;
@@ -29,15 +29,27 @@ const ButtonBase = styled.button`
 const PrimaryButton = styled(ButtonBase)`
   background-color: #F9A109;
 `
+
 const SecondaryButton = styled(ButtonBase)`
   background-color: #56CCF2;
 `
+
 const WarningButton = styled(ButtonBase)`
   background-color: hsla(0, 79%, 63%, 1);
 `
+
 const GhostButton = styled(ButtonBase)`
   color: black;
   background-color: transparent;
+`
+
+const SelectButton = styled.button`
+  border: 1px solid hsla(38, 95%, 51%, 1);
+  border-radius: 24px;
+  background-color: transparent;
+  color: hsla(38, 95%, 51%, 1);
+  font-size: 12px;
+  padding: 10px 20px;
 `
 
 export default Button;
