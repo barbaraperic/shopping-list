bee35e921a3853ef177a2f6874b085789e21c825
Barbara Peric <b.peric10@gmail.com>
2021-06-14 12:23:43 +0200

    WIP: Select dropdown

--- src/components/Input/TagInput.js
+++ src/components/Input/TagInput.js
@@ -6,7 +6,6 @@ export const TagInput = () => {
   const [tags, setTags] = useState([]); 
 
   const onKeyDown = (e) => {
-    console.log(e)
     const { key } = e
     const trimmedInput = input.trim();
 
@@ -15,10 +14,19 @@ export const TagInput = () => {
       setInput('');
     }
   }
+
+  const deleteTag = (index) => {
+    setTags(prevState => prevState.filter((item, i) => i !== index));
+  }
   
   return (
     <Wrapper>
-      {tags.map(tag => <div key={tag}>{tag}</div>)}
+      {tags.map((tag, index) => (
+        <Tag key={tag}>
+          {tag}
+        <button onClick={() => deleteTag(index)}>x</button>
+        </Tag>)
+      )}
       <InputText 
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={onKeyDown}
@@ -28,6 +36,46 @@ export const TagInput = () => {
   )
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  overflow: scroll;
+  width: 309px;
+  /* max-width: 100%; */
+  padding-left: 14px;
+  border: 1px grey solid;
+  border-radius: 5px;
+  color: black;
+  margin: 50px;
+`
+const InputText = styled.input`
+  width: 100%;
+  min-width: 50%;
+  border: none;
+  background-color: transparent;
+  border-radius: 5px;
+  padding: 14px;
+  padding-left: 14px;
+`
 
-const Wrapper = styled.div``
-const InputText = styled.input``
+const Tag = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 7px 0;
+  margin-right: 10px;
+  padding: 0 10px;
+  padding-right: 5px;
+  border: 1px solid orange;
+  border-radius: 5px;
+  background-color: orange;
+  white-space: nowrap;
+  color: white;
+
+  button {
+    display: flex;
+    padding: 6px;
+    border: none;
+    background-color: unset;
+    cursor: pointer;
+    color: white;
+  }
+`
