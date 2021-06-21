56a1b15b32d28cb8d1e31b6fcaeb94e13e9d9826
Barbara Peric <b.peric10@gmail.com>
2021-06-21 15:17:19 +0200

    WIP: line chart d3

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,41 +1,73 @@
 import { useEffect } from 'react';
 import * as d3 from "d3";
-import { chartData } from '../../mock-api/chartData';
+import { data } from '../../mock-api/chartData';
 
 const Chart = () => {
-
-  const drawChart = () => {
-    d3.json(chartData, (d) => {
-      console.log(d)
-    })
-  }
   
-  // const drawChart = () => {
-  //   const dimensions = {
-  //     width: 500,
-  //     height: 300,
-  //     margin: {
-  //       top: 115,
-  //       right: 20,
-  //       bottom: 40,
-  //       left: 60,
-  //     },
-  //   };
+  const drawChart = () => {
+    const width = 500;
 
-  //   dimensions.boundedWidth =
-  //   dimensions.width - dimensions.margin.left - dimensions.margin.right;
-  //   dimensions.boundedHeight =
-  //   dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+    const parseTime = d3.timeParse("%b %d, %Y");
 
-  //   const svg = d3
-  //   .select("body")
-  //   .append("svg")
-  //   .attr("width", dimensions.width)
-  //   .attr("height", dimensions.height)
-  //   .append("g")
-  //   .style("transform",
-  //     `translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`
-  //   );
+    const dates = [];
+    for (let obj of data) {
+      dates.push(parseTime(obj.date));
+    }
+    const domain = d3.extent(dates);
+
+    const xScale = d3.scaleTime().domain(domain).range([0, width])
+
+    console.log(xScale)
+
+    // const yScale = d3.scaleLinear().domain(d3.extent(chartData, d => d.quantity))
+    
+    // const line = d3.line()
+      // .x((d) => { return xScale(d.month)})
+      // .y((d) => { return yScale(d.quantity)})
+
+
+      const xAxis = d3.axisBottom(xScale);
+
+      const svg = d3.select("#my_dataviz");
+
+      svg.append("g")
+      .attr("transform", "translate(0,60)")
+      .call(xAxis);
+
+    // console.log(line(chartData))
+
+    // const xValue = chartData => chartData.month;
+    // const xAxisLabel = 'Month';
+    
+    // const yValue = chartData => chartData.quantity;
+    // const yAxisLabel = 'Quantity';
+
+    // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+      
+    //   function(d) {
+    //     console.log('d',d)
+    //   }
+    // )
+  }
+
+    // const x = d3.scaleTime()
+    //   .domain(d3.extent(chartData.map(item => item.month)))
+
+    // const y = d3.scaleLinear()
+    //   .domain()
+
+    // svg.append("path")
+    //   .datum(data)
+    //   .attr("fill", "none")
+    //   .attr("stroke", "steelblue")
+    //   .attr("stroke-width", 1.5)
+    //   .attr("d", d3.line()
+    //     .x(function(d) { return x(d.month) })
+    //     .y(function(d) { return y(d.quantity) })
+    //     )
+
+
+
 
   //   d3.json(chartData, 
   //     function(d) {
@@ -80,4 +112,32 @@ const Chart = () => {
   return <div id="my_dataviz"></div>
 }
 
-export default Chart
+export default Chart;
+
+
+// const dimensions = {
+//   width: 500,
+//   height: 300,
+//   margin: {
+//     top: 115,
+//     right: 20,
+//     bottom: 40,
+//     left: 60,
+//   },
+// };
+
+// dimensions.boundedWidth =
+// dimensions.width - dimensions.margin.left - dimensions.margin.right;
+// dimensions.boundedHeight =
+// dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+
+// const svg = d3
+//     .select("body")
+//     .append("svg")
+//     .attr("width", dimensions.width)
+//     .attr("height", dimensions.height)
+//     .append("g")
+//     .style("transform",
+//       `translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`
+//     );
--- src/mock-api/chartData.js
+++ src/mock-api/chartData.js
@@ -1,26 +1,35 @@
-export const chartData = [
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-  {
-    "quantity": "35",
-    "month": "January"
-  },
-]
+// export const chartData = [
+//   {
+//     "quantity": 35,
+//     "month": "2021-05-21"
+//   },
+//   {
+//     "quantity": 45,
+//     "month": "2021-06-21"
+//   },
+//   {
+//     "quantity": 50,
+//     "month": "2021-07-21"
+//   },
+//   {
+//     "quantity": 45,
+//     "month": "2021-08-21"
+//   },
+//   {
+//     "quantity": 30,
+//     "month": "2021-09-21"
+//   },
+//   {
+//     "quantity": 35,
+//     "month": "2021-10-21"
+//   },
+// ]
+
+
+export const data = [
+  {"month": "Apr 1, 1970", "quantity": "A"},
+  {"month": "Jun 15, 1971", "quantity": "B"},
+  {"month": "Mar 30, 1972", "quantity": "C"},
+  {"month": "Jan 1, 1973", "quantity": "D"},
+  {"month": "Jun 15, 1973", "quantity": "E"}
+];
