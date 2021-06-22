0770b9a26651d9c04cc2fd5da5d84f5f3cea3176
Barbara Peric <b.peric10@gmail.com>
2021-06-22 10:55:18 +0200

    FEAT: d3 linechart component

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -9,29 +9,24 @@ const Chart = () => {
     
     const width = 400;
     const height = 200;
-    const margin = {top: 10, right: 30, bottom: 30, left: 60};
+    const margin = {top: 10, right: 29, bottom: 30, left: 60};
 
     const svg = d3.select("#my_dataviz")
                   .append("svg")
-                    .attr("width", 400)
-                    .attr("height", 600)
+                    .attr("width", 500)
+                    .attr("height", 300)
                   .append("g")
                     .attr("transform",
                         "translate(" + margin.left + "," + margin.top + ")");
 
-    const min = d3.min(data, d => d.date)
-    const max = d3.max(data, d => d.date)
-
-    const xExtent = d3.extent(data, d => d.date)
-    const extent = d3.extent(data, d => d.quantity);
-
-    console.log(xExtent)
 
     const xScale = d3.scaleBand()
       .domain(data.map(function(d) { 
         return d.date
       }))
       .range([0, width])
+      .padding(-1);
+      // .range([0, width])
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(xScale))
@@ -40,7 +35,7 @@ const Chart = () => {
       .domain([0, d3.max(data, function(d) { 
         return d.quantity 
       })])
-      .rangeRound([height, 0])
+      .range([height, 0])
     svg.append("g")
       .call(d3.axisLeft(yScale).ticks(5));
 
@@ -49,13 +44,13 @@ const Chart = () => {
       .y(d => yScale(d.quantity))
       .curve(d3.curveNatural);
 
-    
     svg.append("path")
         .datum(data)
         .attr("class", "line")
         .attr("fill", "none")
         .attr("stroke", `${COLORS.primary}`)
         .attr("stroke-width", 1.5)
+        .attr("transform", "translate(67, 0)")
         .attr("d", highLine);
   }
   
--- src/mock-api/chartData.js
+++ src/mock-api/chartData.js
@@ -1,36 +1,10 @@
-// export const chartData = [
-//   {
-//     "quantity": 35,
-//     "month": "2021-05-21"
-//   },
-//   {
-//     "quantity": 45,
-//     "month": "2021-06-21"
-//   },
-//   {
-//     "quantity": 50,
-//     "month": "2021-07-21"
-//   },
-//   {
-//     "quantity": 45,
-//     "month": "2021-08-21"
-//   },
-//   {
-//     "quantity": 30,
-//     "month": "2021-09-21"
-//   },
-//   {
-//     "quantity": 35,
-//     "month": "2021-10-21"
-//   },
-// ]
 
-// new Date('01-01-2021')
 export const data = [
-  { date: 'Jan', quantity: 35},
-  { date: 'Feb', quantity: 75},
-  { date: 'Mar', quantity: 49},
-  { date: 'Apr', quantity: 102},
-  { date: 'May', quantity: 140},
-  { date: 'Jun', quantity: 75}
+  { date: 'January', quantity: 35},
+  { date: 'February', quantity: 120},
+  { date: 'March', quantity: 35},
+  { date: 'April', quantity: 15},
+  { date: 'May', quantity: 35},
+  { date: 'June', quantity: 20},
+  { date: 'July', quantity: 45}
 ];
