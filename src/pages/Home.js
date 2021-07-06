734a8ed8019a7985839895c4ea9bb49ca9b26099
Barbara Peric <b.peric10@gmail.com>
2021-07-06 10:55:29 +0200

    FEAT: food api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,14 @@ import Navigation from '../containers/Navigation'
 import Main from '../containers/Main'
 import { AddItemSidebar, PreviewSidebar } from '../containers/Sidebar'
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`
+const API_ID = `${process.env.REACT_APP_API_ID}`
+const ITEM = 'avocado';
+
+fetch(`https://api.edamam.com/api/food-database/v2/parser?app_id=${API_ID}&app_key=${API_KEY}&ingr=${ITEM}&nutrition-type=cooking
+`)
+  .then(response => response.json())
+  .then(data => console.log(data))
 
 const Home = () => {
   const [ toggleSidebar, setToggleSidebar ] = useState(false);
